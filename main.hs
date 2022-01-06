f382a58c106eca093711e4c305bc398a4f1132d9
Bastien <bastien.boymond@epitech.eu>
2022-01-06 18:22:06 +0100

    add of sa sb sc

--- main.hs
+++ main.hs
@@ -9,3 +9,19 @@ myCheckListSort [] = True
 myCheckListSort [a] = True
 myCheckListSort (a:b:xs) | a > b = False
 myCheckListSort (a:as) = myCheckListSort as
+
+sa :: [Int] -> [Int]
+sa [] = []
+sa [a] = [a]
+sa (a:b:as) = b:a:as
+
+sb :: [Int] -> [Int]
+sb  [] = []
+sb [a] = [a]
+sb (a:b:as) = b:a:as
+
+sc :: [Int] -> [Int] -> ([Int], [Int])
+sc [] [] = ([], [])
+sc a [] = (sa a, [])
+sc [] a = ([], sb a)
+sc a b = (sa a, sb b)
