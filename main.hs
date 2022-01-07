58727b7c63dc41482448556c127d487a6088792c
Bastien <bastien.boymond@epitech.eu>
2022-01-07 15:07:34 +0100

    rra rrb et rr done

--- main.hs
+++ main.hs
@@ -29,12 +29,12 @@ sc a b = (sa a, sb b)
 pa :: [Int] -> [Int] -> ([Int], [Int])
 pa [a] [] = ([a], [])
 pa [] [] = ([], [])
-pa (a:b) (c:d) = ((c:a:b), (d))
+pa (a:b) (c:d) = (c:a:b, d)
 
 pb :: [Int] -> [Int] -> ([Int], [Int])
-pb [a] [] = ([a], [])
+pb [] [b] = ([], [b])
 pb [] [] = ([], [])
-pb (a:b) (c:d) = ((b), (a:c:d))
+pb (a:b) (c:d) = (b, a:c:d)
 
 ra :: [Int] -> [Int]
 ra [] = []
@@ -51,3 +51,38 @@ rr [] [] = ([], [])
 rr a [] = (ra a, [])
 rr [] a = ([], rb a)
 rr a b = (ra a, rb b)
+
+rra :: [Int] -> [Int]
+rra [] = []
+rra [a] = [a]
+rra (a:as) = myLast (a:as) : myInit (a:as)
+
+rrb :: [Int] -> [Int]
+rrb [] = []
+rrb [a] = [a]
+rrb (a:as) = myLast (a:as) : myInit (a:as)
+
+rrr :: [Int] -> [Int] -> ([Int], [Int])
+rrr [] [] = ([], [])
+rrr a [] = (rra a, [])
+rrr [] a = ([], rrb a)
+rrr a b = (rra a, rrb b)
+
+myTake :: Int -> [a] -> [a]
+myTake i [] = error "The list is empty"
+myTake i (a:as) | myLength (a:as) < i = a:as
+myTake i (a:as) | i <= 0 = []
+myTake i (a:as) = a:myTake (i - 1) as
+
+myInit :: [a] -> [a]
+myInit [] = error "The list is empty"
+myInit (a:as) = myTake (myLength (a:as) - 1) (a:as)
+
+myLast :: [a] -> a
+myLast [] = error "The list is empty"
+myLast [a] = a
+myLast (a:as) = myLast as
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (a: as) = 1 + myLength as
