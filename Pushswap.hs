b064e6af40774349a14cc3930103aa97424eef45
Bartuzel <tom.bartuzel@epitech.eu>
2022-01-08 02:08:52 +0100

    [origin] maybe int into tuple

--- Pushswap.hs
+++ Pushswap.hs
@@ -1,8 +1,8 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Pushswap where
 import System.Environment
 import Data.Char
-import Data.List.Split
+-- import Data.List.Split
 import System.Exit
 import System.IO
 import Data.Maybe
@@ -18,86 +18,90 @@ myCheckListSort [a] = True
 myCheckListSort (a:b:xs) | a > b = False
 myCheckListSort (a:as) = myCheckListSort as
 
-sa :: [Int] -> [Int]
-sa [] = []
-sa [a] = [a]
-sa (a:b:as) = b:a:as
+sa :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
+sa [] [] = ([],[])
+sa [a] [] = ([a], [])
+sa (a:b:as) [] = (b:a:as, [])
 
-sb :: [Int] -> [Int]
-sb  [] = []
-sb [a] = [a]
-sb (a:b:as) = b:a:as
+sb :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
+sb [] [] = ([],[])
+sb [] [a] = ([], [a])
+sb [] (a:b:as) = ([], b:a:as)
 
-sc :: [Int] -> [Int] -> ([Int], [Int])
+sc :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
 sc [] [] = ([], [])
-sc a [] = (sa a, [])
-sc [] a = ([], sb a)
-sc a b = (sa a, sb b)
+sc a [] = sa a []
+sc [] a = sb [] a
+sc a b = sa a [] >> sb [] b
 
-pa :: [Int] -> [Int] -> ([Int], [Int])
+pa :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
 pa [a] [] = ([a], [])
 pa [] [] = ([], [])
 pa (a:b) (c:d) = (c:a:b, d)
 pa _ _ = ([], [])
 
-pb :: [Int] -> [Int] -> ([Int], [Int])
+pb :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
 pb [] [b] = ([], [b])
 pb [] [] = ([], [])
 pb (a:b) (c:d) = (b, a:c:d)
 pb _ _ = ([], [])
 
-ra :: [Int] -> [Int]
-ra [] = []
-ra [a] = [a]
-ra (a:b:as) = b:as ++ [a]
+ra :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
+ra [] [] = ([], [])
+ra [a] [] = ([a], [])
+ra (a:b:as) [] = (b:as ++ [a], [])
 
-rb :: [Int] -> [Int]
-rb [] = []
-rb [a] = [a]
-rb (a:b:as) = b:as ++ [a]
+rb :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
+rb [] [] = ([], [])
+rb [] [a] = ([], [a])
+rb [] (a:b:as) = ([], b:as ++ [a])
 
-rr :: [Int] -> [Int] -> ([Int], [Int])
+rr :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
 rr [] [] = ([], [])
-rr a [] = (ra a, [])
-rr [] a = ([], rb a)
-rr a b = (ra a, rb b)
+rr a [] = ra a []
+rr [] a = rb [] a
+rr a b = ra a [] >> rb [] b
 
-rra :: [Maybe Int] -> [Maybe Int]
-rra [] = []
-rra [a] = [a]
-rra (a:as) = last (a:as) : init (a:as)
+rra :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
+rra [] [] = ([], [])
+rra [a] [b] = ([a], [b])
+rra (a:as) [b] = (last (a:as) : init (a:as), [b])
 
-rrb :: [Maybe Int] -> [Maybe Int]
-rrb [] = []
-rrb [a] = [a]
-rrb (a:as) = last (a:as) : init (a:as)
+rrb :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
+rrb [] [] = ([], [])
+rrb [a] [b] = ([a], [b])
+rrb [a] (b:bs) = ([a], last (b:bs) : init (b:bs))
 
 rrr :: [Maybe Int] -> [Maybe Int] -> ([Maybe Int], [Maybe Int])
 rrr [] [] = ([], [])
-rrr a [] = (rra a, [])
-rrr [] a = ([], rrb a)
-rrr a b = (rra a, rrb b)
+rrr a [] = rra a []
+rrr [] a = rrb [] a
+rrr a b = rra a [] >> rrb [] b
 
 argsIntToIntList :: [String] -> [Maybe Int]
 argsIntToIntList [] = []
 argsIntToIntList (a:as) = map readMaybe (a:as)
 
-getAction :: String -> ([Maybe Int] -> [Maybe Int])
+getAction :: String -> ([Maybe Int],[Maybe Int])  -> ([Maybe Int], [Maybe Int])
 getAction _ = rrb
 
-findActions :: [String] -> ([Maybe Int], [Maybe Int])-> [Maybe Int]
-findActions ("sa":b)  ((a : c): as) = getAction "sa" (a:as)
-findActions ("sb":b)  (a:as) = getAction "sb" (a:as)
-findActions ("sc":b)  (a:as) = getAction "sc" (a:as)
-findActions ("pa":b)  (a:as) = getAction "pa" (a:as)
-findActions ("pb":b)  (a:as) = getAction "pb" (a:as)
-findActions ("ra":b)  (a:as) = getAction "ra" (a:as)
-findActions ("rb":b)  (a:as) = getAction "rb" (a:as)
-findActions ("rr":b)  (a:as) = getAction "rr" (a:as)
-findActions ("rra":b)  (a:as) = getAction "rra" (a:as)
-findActions ("rrb":b)  (a:as) = getAction "rrb" (a:as)
-findActions ("rrr":b)  (a:as) = getAction "rrr" (a:as)
-findActions _ _ = []
+findActions :: [String] -> ([Maybe Int], [Maybe Int]) -> ([Maybe Int], [Maybe Int])
+findActions ("sa":b)  (a : c, as) = getAction "sa" (a : c, as)
+findActions ("sb":b)  (a : c, as)  = getAction "sb" (a : c, as)
+findActions ("sc":b)  (a : c, as)  = getAction "sc" (a : c, as)
+findActions ("pa":b)  (a : c, as)  = getAction "pa" (a : c, as)
+findActions ("pb":b)  (a : c, as)  = getAction "pb" (a : c, as)
+findActions ("ra":b)  (a : c, as)  = getAction "ra" (a : c, as)
+findActions ("rb":b)  (a : c, as)  = getAction "rb" (a : c, as)
+findActions ("rr":b)  (a : c, as)  = getAction "rr" (a : c, as)
+findActions ("rra":b) (a : c, as)  = getAction "rra" (a : c, as)
+findActions ("rrb":b) (a : c, as)  = getAction "rrb" (a : c, as)
+findActions ("rrr":b) (a : c, as)  = getAction "rrr" (a : c, as)
+findActions _ _ = ([], [])
+
+doProcess :: [String] -> ([Maybe Int], [Maybe Int]) -> [Maybe Int]
+doProcess [] b = []
+doProcess (a:xs) b = doProcess xs (findActions (a:xs) b)
 
 main :: IO ()
 main = do
@@ -107,4 +111,4 @@ main = do
     let actions = myStwa actionsStr
     if Nothing `elem` intList then exitWith(ExitFailure 84)
     else putStr ""
-    if myCheckListSort (findActions actions intList) then print "OK" >> exitSuccess else print "KO" >> exitWith(ExitFailure 84)
+    if myCheckListSort (doProcess actions (intList, [])) then print "OK" >> exitSuccess else print "KO" >> exitWith(ExitFailure 84)
